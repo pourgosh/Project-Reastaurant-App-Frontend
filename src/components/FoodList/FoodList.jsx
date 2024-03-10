566f2eb2b6fb933cac8fc1e77b532e3c3762040d
pouria pou <pouriap024@yahoo.com>
2024-03-10 10:52:49 +0100

    restyle homepage

--- src/components/FoodList/FoodList.jsx
+++ src/components/FoodList/FoodList.jsx
@@ -1,8 +1,16 @@
 import EditFoodListForm from "./FoodListForm/EditFoodListForm";
 import FoodListForm from "./FoodListForm/FoodListForm";
 import { useState } from "react";
+import "./foodList.css";
 
-const FoodList = ({ foodList, deleteFoodOnClick }) => {
+const FoodList = ({
+  foodList,
+  deleteFoodOnClick,
+  WrapperWrapperClassName,
+  wrapperClassName,
+  imageContainer,
+  infoTextContainer,
+}) => {
   const [showCreateForm, setShowCreateForm] = useState(false);
   const [elemToShow, setElemToShow] = useState(null);
 
@@ -16,7 +24,10 @@ const FoodList = ({ foodList, deleteFoodOnClick }) => {
   };
 
   return (
-    <div style={{ margin: "10px" }}>
+    <div
+      style={{ margin: "10px" }}
+      className={WrapperWrapperClassName && WrapperWrapperClassName}
+    >
       {parameter === "/admin/control" && (
         <div style={{ margin: "10px" }}>
           <p onClick={formControler}>Create new Food</p>
@@ -28,47 +39,50 @@ const FoodList = ({ foodList, deleteFoodOnClick }) => {
       {foodList &&
         foodList.map((elem) => {
           return (
-            <div key={elem._id} style={{ margin: "10px" }}>
-              <p>title: {elem.title}</p>
-              {elem.description && <p>description: {elem.description}</p>}
-              {elem.origin && <p>origin: {elem.origin}</p>}
-              {elem.ingredients && <p>ingredients: {elem.ingredients}</p>}
+            <div
+              key={elem._id}
+              style={{ margin: "10px" }}
+              className={wrapperClassName && wrapperClassName}
+            >
+              <p> {elem.title}</p>
               {elem.image && (
-                <div>
-                  <p>image:</p>
+                <div className={imageContainer && imageContainer}>
                   <img src={elem.image} alt="item image" />
                 </div>
               )}
-              {elem.chefsRecommendations && (
-                <p>chefsRecommendations: {elem.chefsRecommendations}</p>
-              )}
-              {elem.category && <p>category: {elem.category}</p>}
-              <p>{elem.price}$</p>
-              <div>
-                {parameter === "/admin/control" && (
-                  <div>
-                    <div>
-                      <p
-                        onClick={() => {
-                          deleteFoodOnClick(elem);
-                        }}
-                      >
-                        Delete Food
-                      </p>
-                    </div>
-
-                    <div>
-                      <p
-                        onClick={() => {
-                          editFormControler(elem._id);
-                        }}
-                      >
-                        Update Food
-                      </p>
-                    </div>
-                  </div>
+              <div className={infoTextContainer && infoTextContainer}>
+                {elem.description && <p>description: {elem.description}</p>}
+                {elem.origin && <p>origin: {elem.origin}</p>}
+                {elem.ingredients && <p>ingredients: {elem.ingredients}</p>}
+                {elem.chefsRecommendations && (
+                  <p>chefsRecommendations: {elem.chefsRecommendations}</p>
                 )}
+                {elem.category && <p>category: {elem.category}</p>}
+                <p>{elem.price}$</p>
               </div>
+              {parameter === "/admin/control" && (
+                <div>
+                  <div>
+                    <p
+                      onClick={() => {
+                        deleteFoodOnClick(elem);
+                      }}
+                    >
+                      Delete Food
+                    </p>
+                  </div>
+
+                  <div>
+                    <p
+                      onClick={() => {
+                        editFormControler(elem._id);
+                      }}
+                    >
+                      Update Food
+                    </p>
+                  </div>
+                </div>
+              )}
               {elemToShow === elem._id && (
                 <EditFoodListForm
                   requestType="put"
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,28 @@
 import "./homePage.css";
 import burgerImg from "../../assets/Images/hamburger(1).jpg";
 import burgerImgTwo from "../../assets/Images/hamburger(3).jpg";
+import FoodList from "../../components/FoodList/FoodList";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_URL } from "../../../ApiUrl";
 
 const HomePage = () => {
   const myText = `Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the classical author and philosopher Cicero. Its words and letters have been changed by addition or removal, so to deliberately render its content nonsensical; it's not genuine, correct, or comprehensible Latin anymore. While lorem ipsum's still resembles classical Latin, it actually has no meaning whatsoever. As Cicero's text doesn't contain the letters K, W, or Z, alien to latin, these, and others are often inserted randomly to mimic the typographic appearence of European languages, as are digraphs not to be found in the original.`;
   const TEXT = "Enjoy!";
+  const [foodList, setFoodList] = useState(null);
+
+  const getFoodsFromDb = async () => {
+    try {
+      const result = await axios.get(`${API_URL}/food`);
+      setFoodList(result.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    getFoodsFromDb();
+  }, []);
+
   return (
     <>
       <div className="mainWrapper">
@@ -26,7 +44,15 @@ const HomePage = () => {
         <section className="menuTitleContainer">
           <p>Our Menu</p>
         </section>
-        <div className="menuContainer"></div>
+        <div className="menuContainer">
+          <FoodList
+            foodList={foodList}
+            WrapperWrapperClassName="foodWrapperWrapper"
+            wrapperClassName="foodItemWrapper"
+            imageContainer="foodImgContainer"
+            infoTextContainer="foodInfoTxtContainer"
+          />
+        </div>
       </div>
       <div className="aboutUsWrapper">
         <div className="aboutUsTextWrapper">
